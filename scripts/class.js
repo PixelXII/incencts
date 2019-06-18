b4a0ffa51ad66852ab157f000bf9fe60e57cdab6
altD <35429528+PixelXII@users.noreply.github.com>
2019-06-18 07:53:49 -0700

    update to latest version with proper movement and matter.js

--- scripts/class.js
+++ scripts/class.js
@@ -2,28 +2,57 @@ class Player {
   constructor(img) {
     this.img = img
     this.acceleration = createVector(0, 0)
+    this.velocity = createVector(0, 0)
     this.position = createVector(200, 200)
-    this.speed = 0.5
+    this.speed = 4;
+    this.physicsBody = Bodies.rectangle(this.position.x, this.position.y, 43, 112)
+    this.maxspeed = 6;
+    World.add(world, this.physicsBody)
   }
   update() { // also the show function
-    this.position.add(this.acceleration)
-    image(this.img, this.position.x, this.position.y)
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxspeed);
+    this.position.add(this.velocity);
+
+    image(this.img, this.position.x, this.position.y);
+    this.physicsBody.position.x = this.position.x
+    this.physicsBody.position.y = this.position.y
   }
-  move(key) {
-    key = key.keyCode
-    if (key === Game.up) {
-      this.acceleration.y += this.speed
+  move() {
+    let acc = this.acceleration
+    let sp = this.speed
+    let vl = this.velocity
+    let p = this;
+    onkeydown = onkeyup = function(key) {
+      key = key.keyCode
+      // if (key === Game.controls.up) {
+      //   acc.y -= sp
+      // }
+      // if (key === Game.controls.down) {
+      //   acc.y += sp
+      // }
+      if (key === Game.controls.left) {
+        acc.x -= sp
+      }
+      if (key === Game.controls.right) {
+        acc.x += sp
+      }
+      if (key === Game.controls.up) {
+        p.jump()
+      }
     }
-    if (key === Game.down) {
-      this.acceleration.y -= this.speed
+    onkeyup = function() {
+      vl.sub(vl)
+      acc.sub(acc)
     }
-    if (key === Game.left) {
-      this.acceleration.x += this.speed
-    }
-    if (key === Game.right) {
-      this.acceleration.x -= this.speed
-    }
+  }
+  playAnim() {
+    // change this.img based on animation frame paths in array
+  }
+  jump() {
+    this.acceleration.y -= this.speed/1.5
+    setTimeout(() => this.acceleration.y += this.speed, 250)
+    setTimeout(() => this.acceleration.y = 0, 300)
   }
 }
-
-  // other classes? Enemy, Friendly (both need some kind of pathfinding tho)
+// other classes? Enemy, Friendly (both need some kind of pathfinding tho)
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,49 +1,61 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+
+
 var Game = {
-    character: {
-        url: "https://cdn.jsdelivr.net/gh/PixelXII/incencts/assets/chara.png",
-        img: null
-    },
-    controls: {
-        up: 38,
-        down: 40,
-        left: 37,
-        right: 39
-    },
-    player: null
+  character: {
+    url: "https://cdn.jsdelivr.net/gh/PixelXII/incencts/assets/evensmallerchar.png", // 43 x 112
+    img: null
+  },
+  controls: {
+    up: 38,
+    down: 40,
+    left: 37,
+    right: 39
+  },
+  player: null
 }
 
+let engine, world;
 
 async function getGameData() {
   return fetch('./data.json')
-  .then(response => {
-    return response.json();
-  })
-  .then(json => {
-    Game = json
-    // loading images & sounds
-    Game.chara = loadImage(Game.character)
-  })
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      Game = json
+      // loading images & sounds
+      Game.chara = loadImage(Game.character)
+    })
 }
 
+
 function preload() {
-    // getGameData()
-    Game.character.img = loadImage(Game.character.url)
+  // getGameData()
+  Game.character.img = loadImage(Game.character.url)
 }
 
 function setup() {
-    createCanvas(800, 600); // final is 800x600
-    Game.player = new Player(Game.character.img)
-    Game.character.img.resize(0, 110)
-    window.addEventListener("keydown", Game.player.move(key))
-    window.addEventListener("keyup", Game.player.move(key))
+  engine = Engine.create()
+  world = engine.world;
+  world.gravity.y = 1;
+
+  createCanvas(400, 400); // final is 800x600
+  Game.player = new Player(Game.character.img)
+  // Game.character.img.resize(0, 110)
+  window.addEventListener("keydown", Game.player.move(key))
+  window.addEventListener("keyup", Game.player.move(key))
+  frameRate(120)
 }
 
 function draw() {
-    background(220);
-    Game.player.update()
-    onkeydown = onkeyup = function(key) {
-      Game.player.move(key.keyCode)
-    }
+  background(220);
+  Engine.update(engine)
+  Game.player.update()
+  Game.player.move()
 }
 
 setTimeout(() => console.log(Game), 800)
